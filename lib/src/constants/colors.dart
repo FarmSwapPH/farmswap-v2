7abd1a8aac9802d8f6ae4d358e0908d1a96d5a93
CodePhilanthropist <66899966+PoorCoderCreations@users.noreply.github.com>
2023-09-08 03:20:15 +0800

    featAdd: Edit FarmSwapInput with Cursor Green

---
+++ lib/src/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class FarmSwapGreen {
+  static Color lightGreen = const Color(0xFFe8f9f1);
+  static Color lightGreenHover = const Color(0xFFdcf5eb);
+  static Color lightGreenActive = const Color(0xFFb6ebd5);
+  static Color normalGreen = const Color(0xFF15be77);
+  static Color normalGreenHover = const Color(0xFF13ab6b);
+  static Color normalGreenActive = const Color(0xFF11985f);
+  static Color darkGreen = const Color(0xFF108f59);
+  static Color darkGreenHover = const Color(0xFF0d7247);
+  static Color darkGreenActive = const Color(0xFF095536);
+  static Color darkGreenDarker = const Color(0xFF07432a);
+}
--- lib/src/features/authentication/presentation/register_screen.dart
+++ lib/src/features/authentication/presentation/register_screen.dart
@@ -25,149 +25,154 @@ class _RegisterScreenState extends State<RegisterScreen> {
       resizeToAvoidBottomInset: true,
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: height * 0.36,
-                width: width,
-                child: Stack(
+          child: SizedBox(
+            height: height,
+            width: width,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: height * 0.36,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 0,
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: SvgPicture.asset(
+                          "assets/svg/auth/Pattern.svg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Positioned(
+                        top: 47,
+                        right: 89,
+                        left: 89,
+                        child: Center(
+                          child: Image.asset("assets/images/Logo.png"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: height * 0.024),
+                Column(
                   children: [
-                    Positioned(
-                      top: 0,
-                      bottom: 0,
-                      left: 0,
-                      right: 0,
-                      child: SvgPicture.asset(
-                        "assets/svg/auth/Pattern.svg",
+                    farmSwapFont(
+                        text: "Sign Up For Free ", size: height * 0.024),
+                    const SizedBox(height: 20),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      child: FarmSwapTextField(
+                        hintText: "Username",
+                        onPress: () {},
+                        inputIcon: "assets/svg/auth/Profile.svg",
                       ),
                     ),
-                    Positioned(
-                      top: 47,
-                      right: 89,
-                      left: 89,
-                      child: Center(
-                        child: Image.asset("assets/images/Logo.png"),
+                    SizedBox(height: height * 0.024),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      child: FarmSwapTextField(
+                        hintText: "Email",
+                        onPress: () {},
+                        inputIcon: "assets/svg/auth/Message.svg",
                       ),
                     ),
+                    SizedBox(height: height * 0.024),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      child: FarmSwapTextField(
+                        hintText: "Password",
+                        onPress: () {},
+                        inputIcon: "assets/svg/auth/Lock.svg",
+                        isPassword: true,
+                      ),
+                    )
                   ],
                 ),
-              ),
-              SizedBox(height: height * 0.024),
-              Column(
-                children: [
-                  farmSwapFont(text: "Sign Up For Free ", size: height * 0.024),
-                  const SizedBox(height: 20),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 20),
-                    child: FarmSwapTextField(
-                      hintText: "Username",
-                      onPress: () {},
-                      inputIcon: "assets/svg/auth/Profile.svg",
-                    ),
-                  ),
-                  SizedBox(height: height * 0.024),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 20),
-                    child: FarmSwapTextField(
-                      hintText: "Email",
-                      onPress: () {},
-                      inputIcon: "assets/svg/auth/Message.svg",
-                    ),
-                  ),
-                  SizedBox(height: height * 0.024),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 20),
-                    child: FarmSwapTextField(
-                      hintText: "Password",
-                      onPress: () {},
-                      inputIcon: "assets/svg/auth/Lock.svg",
-                      isPassword: true,
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(height: height * 0.024),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      agreeToTerms = !agreeToTerms;
-                    });
-                  },
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 20),
-                    width: width,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          width: 22,
-                          height: 22,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            border: Border.all(
+                SizedBox(height: height * 0.024),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        agreeToTerms = !agreeToTerms;
+                      });
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      width: width,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 22,
+                            height: 22,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              border: Border.all(
+                                color: agreeToTerms == true
+                                    ? const Color(0xFF53E78B)
+                                    : Colors.grey,
+                              ),
                               color: agreeToTerms == true
                                   ? const Color(0xFF53E78B)
-                                  : Colors.grey,
+                                  : Colors.white,
                             ),
-                            color: agreeToTerms == true
-                                ? const Color(0xFF53E78B)
-                                : Colors.white,
-                          ),
-                          child: Align(
-                            child:
-                                SvgPicture.asset("assets/svg/auth/check.svg"),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Text(
-                          "Agree to terms & conditions",
-                          style: GoogleFonts.poppins(
-                            textStyle: TextStyle(
-                              color: const Color(0xFF3B3B3B),
-                              fontSize: height * 0.0147,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 0.50,
+                            child: Align(
+                              child:
+                                  SvgPicture.asset("assets/svg/auth/check.svg"),
                             ),
                           ),
-                        ),
-                      ],
+                          const SizedBox(width: 10),
+                          Text(
+                            "Agree to terms & conditions",
+                            style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                                color: const Color(0xFF3B3B3B),
+                                fontSize: height * 0.0147,
+                                fontWeight: FontWeight.w400,
+                                letterSpacing: 0.50,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              const FarmSwapPrimaryButton(
-                buttonTitle: "Create Account",
-                nextScreen: ChooseUserTypeScreen(),
-              ),
-              SizedBox(height: height * 0.012),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: ((context) => const LoginScreen()),
-                    ),
-                  );
-                },
-                child: Text(
-                  'Already have an account?',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.poppins(
-                    textStyle: TextStyle(
-                      color: const Color(0xFF53E78B),
-                      fontSize: height * 0.0147,
-                      fontWeight: FontWeight.w500,
-                      decoration: TextDecoration.underline,
-                      decorationStyle: TextDecorationStyle.solid,
-                      decorationColor: const Color(0xFF53E78B),
+                const SizedBox(height: 20),
+                const FarmSwapPrimaryButton(
+                  buttonTitle: "Create Account",
+                  nextScreen: ChooseUserTypeScreen(),
+                ),
+                SizedBox(height: height * 0.012),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: ((context) => const LoginScreen()),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'Already have an account?',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        color: const Color(0xFF53E78B),
+                        fontSize: height * 0.0147,
+                        fontWeight: FontWeight.w500,
+                        decoration: TextDecoration.underline,
+                        decorationStyle: TextDecorationStyle.solid,
+                        decorationColor: const Color(0xFF53E78B),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
